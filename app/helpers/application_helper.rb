6451ef6f8278a0c090fe036ae774edf68c122161
Ihor Breza <seniorigor@gmail.com>
2016-09-28 10:18:46 +0300

    Generate slack_button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def generate_slack_button
+    image = image_tag("https://platform.slack-edge.com/img/add_to_slack@2x.png",
+      alt: "Add to Slack"
+    )
+
+    params = {
+      scope:        "bot,channels:write,chat:write:user",
+      client_id:    ENV["CLIENT_ID"],
+      redirect_uri: ENV["REDIRECT_URI"],
+      state:        "authorized"
+    }.to_query
+
+    link_to(image, "https://slack.com/oauth/authorize?#{params}")
+  end
 end
